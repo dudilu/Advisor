41419d45b7eede20634233700f15a35e29f48b61
dudilu <92081413+dudilu@users.noreply.github.com>
2024-03-19 18:36:08 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -550,7 +550,7 @@ with st.sidebar:
 
     if selected == "📊 Our Portfolio":
     #   selected_tab = st.selectbox("Select a Period", ["1Y", "0.5Y", "1Q"])
-        1 = 1
+        fff = 1
 
     elif selected == "🚀 Strategic Performance":
         start_date = pd.Timestamp(st.sidebar.date_input("Start date", min_value=performance['Date'].min(),max_value=performance['Date'].max(),value=performance['Date'].min()))
